64434ef334ba1ebdbbf53a947cfc160ecc73730e
enigmalb <enigmalb@outlook.com>
2020-03-02 20:18:12 -0800

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,13 @@ require 'bundler/setup'
 Bundler.require
 
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+ 
+ 
+#ActiveRecord::Base.connection.execute(sql)
 # put the code to connect to the database here
 
 require_relative "../artist.rb"
